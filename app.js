7e5993c271b09bf9fbed2390797119021ef2e7ae
Mohit yadav <elitemohityadav@gmail.com>
2021-10-29 23:39:45 +0530

    Added audio and start button

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+var highscore=0;
+
+audio = new Audio('music.mp3');
 
 function jump(){
     if(character.classList == "animate"){return}
@@ -8,20 +11,42 @@ function jump(){
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
-}
+}function start(){
+block.classList.add("blockani")
+audio.play();
+audio.currentTime = 20;
+counter=0;
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
+        // block.style.animation = "none";
+        block.classList.remove('blockani');
+      audio.pause();
+      audio.currentTime = 20;
+        alert("Game Over. score: "+Math.floor(counter/100)+"\nClick start again");
         counter=0;
-        block.style.animation = "block 1s infinite linear";  
-        
-      
+        document.getElementById("scoreSpan").innerHTML =counter;
+       
+        // block.style.animation = "block 1s infinite linear";  
+       clearInterval(checkDead);
 
     }else{
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);   
+ 
     }
 }, 10);
+}
+
+
+window.addEventListener("keydown",a=>{
+     switch (a.key)
+         {
+     case "ArrowUp":jump();
+    break;
+     default:
+                break;
+ }
+    })
