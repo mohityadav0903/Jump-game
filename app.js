2bf263da6594c3ab6f2bdbc4dd8506a0c1668a1c
Mohit yadav <elitemohityadav@gmail.com>
2021-10-30 17:53:21 +0530

    best and last score added

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
 var highscore=0;
-
+var score=0;
+ highscore = parseInt( localStorage.getItem("bestscore"));
+ document.getElementById("bestscore").innerHTML =highscore;
+ document.getElementById("lastscore").innerHTML =score;
 audio = new Audio('music.mp3');
 
 function jump(){
@@ -12,10 +15,13 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }function start(){
-block.classList.add("blockani")
+block.classList.add("blockani")   
+ 
 audio.play();
 audio.currentTime = 20;
+
 counter=0;
+score=0;
 
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -25,19 +31,31 @@ var checkDead = setInterval(function() {
         block.classList.remove('blockani');
       audio.pause();
       audio.currentTime = 20;
+      
         alert("Game Over. score: "+Math.floor(counter/100)+"\nClick start again");
-        counter=0;
-        document.getElementById("scoreSpan").innerHTML =counter;
        
+        counter=0; 
+        document.getElementById("scoreSpan").innerHTML =counter;
+        document.getElementById("lastscore").innerHTML =score;
         // block.style.animation = "block 1s infinite linear";  
-       clearInterval(checkDead);
+        clearInterval(checkDead);
+    
+   
 
     }else{
+       
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);   
- 
-    }
-}, 10);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);    
+           document.getElementById("bestscore").innerHTML =highscore;
+        score= Math.floor(counter/100);
+    if (score > highscore) {
+        highscore=score;
+        localStorage.setItem("bestscore", highscore);  
+       
+    } 
+}
+}, 10);   
+
 }
 
 
@@ -49,4 +67,6 @@ window.addEventListener("keydown",a=>{
      default:
                 break;
  }
-    })
+    }) 
+
+
